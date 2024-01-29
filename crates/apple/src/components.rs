6dba24e9099a23d63eada863a02dcd573df9ad41
Robert Dodd <robert@dodd.me>
2024-01-29 11:57:37 +1100

    refactor: add comment explaining the reason for "component.rs" files

--- crates/apple/src/components.rs
+++ crates/apple/src/components.rs
@@ -1,3 +1,13 @@
+/// NOTE: The reason we have a separate file only to hold a single type is for consistent type paths in bevy scene
+/// files.
+///
+/// See the "prefab.scn.ron" asset files for examples, the `Apple` type path is "apple::components::Apple".
+///
+/// All (workspace crate) type paths follow the same convention:
+/// "[crate_name]::components::[ComponentName]"
+///
+/// This can become important because if a type path changes, all scene files referencing it will break. It also makes
+/// them predictable, allowing you to write a prefab asset without having to refer to exactly what the type paths are.
 use bevy::prelude::*;
 
 /// An apple.
